aa27a354c1c3979b8bc142dca3baa121f3d22faa
Nils <nilsotto@gmx.de>
2023-09-27 11:56:51 +0200

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Navigation from "./components/Navigation";
 import Bookmarks from "./pages/Bookmarks";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
 import Profil from "./pages/Profil";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
-      <Navigation />
+      <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Bookmarks" element={<Bookmarks />} />
