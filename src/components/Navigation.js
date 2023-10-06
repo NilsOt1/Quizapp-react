bcffff9d0cc95028296d769cfb6f26104a05e152
Nils <nilsotto@gmx.de>
2023-10-06 18:59:06 +0200

    add styled to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,29 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Navigation() {
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/bookmarks">Bookmarks</NavLink>
-      <NavLink to="/form">Form</NavLink>
-      <NavLink to="/profil">Profil</NavLink>
-    </nav>
+    <StyledNav>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/bookmarks">Bookmarks</StyledLink>
+      <StyledLink to="/form">Form</StyledLink>
+      <StyledLink to="/profil">Profil</StyledLink>
+    </StyledNav>
   );
 }
+
+const StyledNav = styled.nav`
+  width: 100%;
+  border: solid 2px;
+  display: flex;
+  position: fixed;
+  bottom: 0px;
+  background-color: white;
+`;
+
+const StyledLink = styled(NavLink)`
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  text-decoration: none;
+`;
